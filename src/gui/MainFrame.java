3ff6a3b6ab62121dc2dbcf82e0b8993c06b11d16
kristh <kris.th@gmail.com>
2014-03-10 18:18:44 +0100

    Added confirmation for closing application

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -2,10 +2,13 @@ package gui;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 import models.Person;
 
@@ -13,21 +16,30 @@ public class MainFrame extends JFrame {
 
 	private JDialog loginDialog;
 	private Person user;
-	
+
 	public MainFrame() {
 		super("Avtalebok");
 		setSize(1024, 768);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				dispose();
-                loginGUI();
-            }
-		});
-		loginGUI();
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		
+		UIManager.put("OptionPane.yesButtonText", "Ja");
+		UIManager.put("OptionPane.noButtonText", "Nei");
+		
+		WindowListener exitListener = new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				int confirm = JOptionPane.showOptionDialog(null, "Er du sikker på at du vil logge ut?", "Bekreftelse", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+				if (confirm == 0) {
+					dispose();
+					loginGUI();
+				}
+			}
+		};
+		addWindowListener(exitListener);
+
+		loginGUI();
+
 	}
-	
+
 	private void loginGUI() {
 		LogInPanel loginPanel = new LogInPanel();
 		loginDialog = new JDialog(this, "Innlogging", true);
@@ -35,7 +47,7 @@ public class MainFrame extends JFrame {
 		loginDialog.setLocationRelativeTo(null); // Place in center of screen
 		loginDialog.add(loginPanel);
 		loginDialog.setVisible(true);
-		
+
 		if(loginPanel.getUser() != null) { // After dialog is dismissed we can set the user
 			user = loginPanel.getUser();
 			// TODO Open main window
@@ -44,9 +56,9 @@ public class MainFrame extends JFrame {
 			System.exit(-1); // Otherwise, shut down
 		}
 	}
-	
+
 	public static void main(String[] args){
 		new MainFrame().setVisible(true);
 	}
-	
+
 }
