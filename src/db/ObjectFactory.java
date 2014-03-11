563ac9ac3230d920ad21ab4b3b089d835a639948
alien93 <alien_93@hotmail.no>
2014-03-11 15:23:48 +0100

    Obj.fact. changes

--- src/db/ObjectFactory.java
+++ src/db/ObjectFactory.java
@@ -3,10 +3,16 @@ package db;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import appointment.Appointment;
+
+import models.Person;
 
 public class ObjectFactory {
 
-	public static void getAllEmployees() {
+	public static ArrayList<Person> getAllEmployees() {
+		ArrayList<Person> retList = new ArrayList<Person>();
 		DBConnection con = new DBConnection("src/db/props.properties");
 		PreparedStatement pst = null;
 		ResultSet rs = null;
@@ -17,8 +23,11 @@ public class ObjectFactory {
 			rs = pst.executeQuery();
 			
 			while (rs.next()) {
-				//TODO returner array med alle employees
+				retList.add(new Person(rs.getString(1)));
 			}
+			
+			return retList;
+			
 		} catch (SQLException e) {
 			throw new RuntimeException("Feil ved uthenting av ansatte!");
 		} finally {
@@ -35,24 +44,43 @@ public class ObjectFactory {
 		}
 	}
 	
-//	public static void getEmployeeAppointments(Person employee) {
-//		DBConnection con = new DBConnection("src/db/props.properties");
-//		PreparedStatement pst;
-//		ResultSet rs;
-//		con.init();
-//		
-//		try {
-//			pst = con.prepareStatement("SELECT AP.*" +
-//					"FROM (appointment AS AP) NATURAL JOIN (employeeappointmentalarm AS EAA)" +
-//					"WHERE EAA.Username = '" + /*employee.getUsername()*/ + "'");
-//			rs = pst.executeQuery();
-//			
-//			while (rs.next()) {
-//				// TODO Do stuff
-//			}
-//		} 
-//	}
-//	
+	public static void getEmployeeAppointments(Person employee) {
+		ArrayList<Appointment> retList = new ArrayList<Appointment>();
+		DBConnection con = new DBConnection("src/db/props.properties");
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		con.init();
+		
+		try {
+			pst = con.prepareStatement("SELECT AP.*" +
+					"FROM (appointment AS AP) NATURAL JOIN (employeeappointmentalarm AS EAA)" +
+					"WHERE EAA.Username = '" + employee.getUsername() + "'");
+			rs = pst.executeQuery();
+			
+			while (rs.next()) {
+				Appointment ap = new Appointment(rs.getInt("AppointmentNumber"),
+						rs.getString("AppointmentName"),
+						rs.getString("StartTime"),
+						rs.getString("EndTime"),
+						rs.getInt("RoomNumber"));
+				retList.add(ap);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Klarte ikke hente employeeAppointments!");
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pst != null)
+					pst.close();
+				con.close();
 
+			} catch (SQLException e) {
+				System.err.println("Kunne ikke lukke alle ressurser!");
+			}
+		}
+	}
+	
+	
 
 }
