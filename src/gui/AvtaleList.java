930a6aef1ccdca32cde10baae3f8ff9d4c391411
kristh <kris.th@gmail.com>
2014-03-13 14:50:21 +0100

    Fixed bugs in database connection

--- src/gui/AvtaleList.java
+++ src/gui/AvtaleList.java
@@ -16,12 +16,12 @@ import db.ObjectFactory;
 
 public class AvtaleList extends JList{
 	private String date;
-	
+
 	public AvtaleList(){
 		this.setCellRenderer(new AvtaleRenderer());
 		this.setModel(new DefaultListModel<Appointment>());
 	}
-	
+
 	public AvtaleList(String date, String employee){
 		this(date, new String[]{employee});
 	}
@@ -29,48 +29,58 @@ public class AvtaleList extends JList{
 		this.date = date;
 		this.setCellRenderer(new AvtaleRenderer());
 		this.setModel(new DefaultListModel<Appointment>());
-		
+
 		fetchApps(employees);
-		
+
 	}
-	
+
 	public void setDate(String date){
 		this.date = date;
 	}
-	
+
 	public void fetchApps(String[] employees){
-			((DefaultListModel<Appointment>) this.getModel()).clear();
-			//Hente avtaler fra databasen
-			String employeesString = "";
-			for (String employee : employees){
-				employeesString += "EAA.Username = '"+employee+"' ";
-				if(!employee.equals(employees[employees.length-1]))employeesString += " OR ";
+		((DefaultListModel<Appointment>) this.getModel()).clear();
+		//Hente avtaler fra databasen
+		String employeesString = "";
+		for (String employee : employees){
+			employeesString += "EAA.Username = '"+employee+"' ";
+			if(!employee.equals(employees[employees.length-1]))employeesString += " OR ";
+		}
+
+		DBConnection connection = null;
+		ResultSet rs = null;
+		try {
+			connection = new DBConnection("src/db/props.properties");
+			connection.init();
+			rs = connection.smallSELECT(	
+					"SELECT AP.* FROM (appointment AS AP) NATURAL JOIN (employeeappointmentalarm AS EAA)" +
+							"WHERE (DATE(AP.StartTime)  = " + "'" +this.date+"'" +
+							"AND ("+employeesString+"))");
+			if (rs.next()) {
+				Appointment app = new Appointment(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
+				app.setStatus(ObjectFactory.getStatus(employees[0], app));
+				((DefaultListModel<Appointment>) this.getModel()).addElement(app);
+
 			}
-			
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
 			try {
-				DBConnection connection = new DBConnection("src/db/props.properties");
-				connection.init();
-				ResultSet rs = connection.smallSELECT(	
-						"SELECT AP.* FROM (appointment AS AP) NATURAL JOIN (employeeappointmentalarm AS EAA)" +
-						"WHERE (DATE(AP.StartTime)  = " + "'" +this.date+"'" +
-								"AND ("+employeesString+"))");
-				while (rs.next()) {
-					Appointment app = new Appointment(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
-					app.setStatus(ObjectFactory.getStatus(employees[0], app));
-					((DefaultListModel<Appointment>) this.getModel()).addElement(app);
-					
-				}
-				rs.close();
-				connection.close();
-				
-			} catch (SQLException e) {
+				if (rs != null)
+					rs.close();
+				if (connection != null)
+					connection.close();
+			}
+			catch (SQLException e) {
 				e.printStackTrace();
 			}
+		}
 	}
 	public void fetchApps(String employee){
 		fetchApps(new String[]{employee});
 	}
-	
+
 	public static void main(String[] args){
 		JFrame frame = new JFrame("testlist");
 		AvtaleList liste = new AvtaleList("2014-03-10", "Anders");
@@ -78,7 +88,7 @@ public class AvtaleList extends JList{
 		frame.setVisible(true);
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 	}
 
 }
