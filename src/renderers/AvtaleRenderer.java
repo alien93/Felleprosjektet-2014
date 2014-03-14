ea67175da50f653d843c1e624e2373d76a04f744
Edeas <endrl@stud.ntnu.no>
2014-03-14 14:13:40 +0100

    Jobber med varselikon-ish

--- src/renderers/AvtaleRenderer.java
+++ src/renderers/AvtaleRenderer.java
@@ -9,6 +9,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -25,6 +26,7 @@ public class AvtaleRenderer extends JPanel implements ListCellRenderer<Appointme
 	JLabel tidText = new JLabel("");
 	JLabel romText = new JLabel("");
 	JLabel vertText = new JLabel("");
+	JLabel varselText = new JLabel("");
 	Border line;
 	
 	public AvtaleRenderer(){
@@ -33,13 +35,22 @@ public class AvtaleRenderer extends JPanel implements ListCellRenderer<Appointme
 		
 		GridBagConstraints gc = new GridBagConstraints();
 		gc.insets = new Insets(1, 5, 1, 5); // Padding
-		gc.anchor = GridBagConstraints.EAST; // Alignment
+		gc.anchor = GridBagConstraints.WEST; // Alignment
 		
 		gc.gridx = 0; // Column
 		gc.gridy = 0; // Row
 		gc.fill = GridBagConstraints.HORIZONTAL;
 		add(nameText, gc);
 		nameText.setFont(new Font(nameText.getFont().getName(), Font.BOLD, 14));
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.EAST;
+		gc.fill = GridBagConstraints.REMAINDER;
+		add(varselText, gc);
+		varselText.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		//varselText.setForeground(new Color(255, 230, 130));
+		varselText.setForeground(Color.BLACK);
+		gc.anchor = GridBagConstraints.WEST;
 		
 		gc.gridx = 0; // Column
 		gc.gridy = 1; // Row
@@ -50,6 +61,7 @@ public class AvtaleRenderer extends JPanel implements ListCellRenderer<Appointme
 		gc.gridx = 1; // Column
 		gc.gridy = 2; // Row
 		gc.fill = GridBagConstraints.HORIZONTAL;
+		gc.fill = GridBagConstraints.REMAINDER;
 		add(romText, gc);
 		romText.setFont(new Font(nameText.getFont().getName(), Font.PLAIN, 11));
 		
@@ -69,6 +81,7 @@ public class AvtaleRenderer extends JPanel implements ListCellRenderer<Appointme
 		tidText.setText(avtale.getStartTime().substring(0, 5) + " - " + avtale.getEndTime().substring(0, 5));
 		romText.setText("Rom: " + String.valueOf(avtale.getMeetingRoomNr()));
 		vertText.setText("Vert: " + "TODO");
+		varselText.setText("*");
 		
 	
 		avtaler.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
