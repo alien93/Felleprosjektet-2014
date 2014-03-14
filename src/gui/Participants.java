234eed4432b97b7dd5ee0c5959ecc73222fc4c1e
kristh <kris.th@gmail.com>
2014-03-14 14:08:46 +0100

    Added constructors for multiple usage

--- src/gui/Participants.java
+++ src/gui/Participants.java
@@ -15,20 +15,20 @@ import java.util.List;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import models.Appointment;
-import db.DBConnection;
-import db.ObjectFactory;
 import models.Person;
 import renderers.PersonRenderer;
+import db.DBConnection;
+import db.ObjectFactory;
 
-public class Participants extends JDialog {
+public class Participants extends JPanel {
 
 	private JTextField searchInput;
 	private JLabel searchResultLabel, attendingLabel;
@@ -37,36 +37,14 @@ public class Participants extends JDialog {
 	private DefaultListModel<Person> unattendingEmployeesModel, attendingEmployeesModel;
 	private ArrayList<Person> unattendingEmployees, attendingEmployees, attendingEmployeesAtLoad, potentialEmployeesToAdd;
 	private JScrollPane searchResultPane, attendingListPane;
+	private Person user;
 
-	public Participants(final JFrame frame, final Appointment ap) {
-		super(frame, "Legg til brukere", true);
-		setLayout(new GridBagLayout());
-		setSize(600, 400);
-		setLocationRelativeTo(null);
-		
-		searchInput = new JTextField(25);
-		searchResultLabel = new JLabel("Employees");
-		attendingLabel = new JLabel("Added");
-		searchResult = new JList<Person>();
-		attendingList = new JList<Person>();
-		attendButton = new JButton(">");
-		undoAttendButton = new JButton("<");
-		saveButton = new JButton("Lagre");
+	public Participants(final JFrame frame, final Appointment ap, final Person person) {
+		//super(frame, "Avtale", true);
+		user = person;
 
-		searchResult.setCellRenderer(new PersonRenderer()); // Add list renderers
-		attendingList.setCellRenderer(new PersonRenderer());
-		
-		searchResultPane = new JScrollPane(searchResult); // Add lists to scrollpanes
-		attendingListPane = new JScrollPane(attendingList);
-
-		searchResultPane.setPreferredSize(new Dimension(150, 150)); // Set pane sizes
-		attendingListPane.setPreferredSize(new Dimension(150, 150));
-		
-		unattendingEmployeesModel = new DefaultListModel<Person>(); // Create models
-		attendingEmployeesModel = new DefaultListModel<Person>();
 		attendingEmployeesAtLoad = new ArrayList<Person>();
-		searchResult.setModel(unattendingEmployeesModel); // Set JList models
-		attendingList.setModel(attendingEmployeesModel);
+		potentialEmployeesToAdd = new ArrayList<Person>();
 
 		// Push employees who's allready attending into attendingEmployeesAtLoad
 		DBConnection con = new DBConnection("src/db/props.properties", true);
@@ -81,55 +59,12 @@ public class Participants extends JDialog {
 			e.printStackTrace();
 		}
 		con.close();
-		
-		// Adding lists containing all unattending and attending employees
-		unattendingEmployees = new ArrayList<Person>();
-		attendingEmployees = new ArrayList<Person>();
 
-		unattendingEmployees = ObjectFactory.getAllEmployees();
-		attendingEmployees = new ArrayList<Person>(attendingEmployeesAtLoad);
-		potentialEmployeesToAdd = new ArrayList<Person>();
-		
-		unattendingEmployees.removeAll(attendingEmployees); // Remove all attending employees from not attending list
-		updateAttendingEmployeesModel(); // Update attending employees model
-		updateUnattendingEmployeesModel(); // Update unattending employees model
-		
-		
+		initGUI(frame);
+
 		// Actionlisteners //
-		searchInput.addKeyListener(new KeyListener() {
-			public void keyTyped(KeyEvent e) { }
-			public void keyReleased(KeyEvent e) {
-				updateUnattendingEmployeesModel(); // Update unattending model
-			}
-			public void keyPressed(KeyEvent e) { }
-		});
-		
-		attendButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				List<Person> selectedEmployees = searchResult.getSelectedValuesList();
-				for (Person p : selectedEmployees) {
-					attendingEmployees.add(p); // Add employee to attending list
-					unattendingEmployees.remove(p); // Remove employee from unattending list
-					if (!potentialEmployeesToAdd.contains(p))
-							potentialEmployeesToAdd.add(p);
-				}
-				updateUnattendingEmployeesModel(); // Update models
-				updateAttendingEmployeesModel();
-			}
-		});
-		undoAttendButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				List<Person> selectedEmployees = attendingList.getSelectedValuesList();
-				for (Person p : selectedEmployees) {
-					unattendingEmployees.add(p);
-					attendingEmployees.remove(p);
-				}
-				updateUnattendingEmployeesModel();
-				updateAttendingEmployeesModel();
-			}
-		});
 		saveButton.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				DBConnection con = new DBConnection("src/db/props.properties", false);
@@ -137,7 +72,7 @@ public class Participants extends JDialog {
 				saveParticipantsOnAttending(con);
 				con.commit();
 				con.close();
-				dispose();
+				//dispose();
 			}
 
 			private void deleteParticipantsNotOnAttending(DBConnection con2) {
@@ -156,7 +91,7 @@ public class Participants extends JDialog {
 					e.printStackTrace();
 					throw new RuntimeException();
 				} 
-				
+
 			}
 
 			private void saveParticipantsOnAttending(DBConnection con2) {
@@ -165,7 +100,7 @@ public class Participants extends JDialog {
 				PreparedStatement prs;
 				try {
 					prs = con2.prepareStatement("INSERT INTO employeeappointmentalarm(Username, AppointmentNumber,Status)" +
-												"VALUES (?,?,?)");
+							"VALUES (?,?,?)");
 					for (Person p : potentialEmployeesToAdd) {
 						prs.setString(1, p.getUsername());
 						prs.setInt(2, ap.getId());
@@ -177,10 +112,114 @@ public class Participants extends JDialog {
 					e.printStackTrace();
 					throw new RuntimeException();
 				}
-				
+
 			}
 		});
 
+		addGUI();
+	}
+	
+	public Participants(final JFrame frame, Person person) {
+		//super(frame, "Legg til brukere", true);
+		user = person;
+		initGUI(frame);
+		
+		// TODO
+		
+		addGUI();
+	}
+
+	public void updateUnattendingEmployeesModel() {
+		unattendingEmployeesModel.clear(); // Clear model list
+		for (Person p : unattendingEmployees) { // Iterate through selected employees
+			if (! unattendingEmployeesModel.contains(p) && p.getUsername().toLowerCase().contains(searchInput.getText().toLowerCase())) {
+				unattendingEmployeesModel.addElement(p); // Add employee to unattending list if the above requirements are fulfilled
+			}
+		}
+	}
+
+	public void updateAttendingEmployeesModel() {
+		attendingEmployeesModel.clear();
+		for (Person p : attendingEmployees) {
+			attendingEmployeesModel.addElement(p);
+		}
+	}
+
+	private void initGUI(JFrame frame) {
+		setLayout(new GridBagLayout());
+		setSize(600, 400);
+		frame.setLocationRelativeTo(null);
+
+		searchInput = new JTextField(25);
+		searchResultLabel = new JLabel("Employees");
+		attendingLabel = new JLabel("Added");
+		searchResult = new JList<Person>();
+		attendingList = new JList<Person>();
+		attendButton = new JButton(">");
+		undoAttendButton = new JButton("<");
+		saveButton = new JButton("Lagre");
+
+		searchResult.setCellRenderer(new PersonRenderer()); // Add list renderers
+		attendingList.setCellRenderer(new PersonRenderer());
+
+		searchResultPane = new JScrollPane(searchResult); // Add lists to scrollpanes
+		attendingListPane = new JScrollPane(attendingList);
+
+		searchResultPane.setPreferredSize(new Dimension(150, 150)); // Set pane sizes
+		attendingListPane.setPreferredSize(new Dimension(150, 150));
+
+		unattendingEmployeesModel = new DefaultListModel<Person>(); // Create models
+		attendingEmployeesModel = new DefaultListModel<Person>();
+
+		searchResult.setModel(unattendingEmployeesModel); // Set JList models
+		attendingList.setModel(attendingEmployeesModel);
+
+		unattendingEmployees = ObjectFactory.getAllEmployees();
+		attendingEmployees = new ArrayList<Person>(attendingEmployeesAtLoad);
+
+		unattendingEmployees.removeAll(attendingEmployees); // Remove all attending employees from not attending list
+		
+		attendingEmployees.remove(user); // Remove logged in user from appointment
+
+		updateAttendingEmployeesModel(); // Update attending employees model
+		updateUnattendingEmployeesModel(); // Update unattending employees model
+		
+		// Actionlisteners //
+		searchInput.addKeyListener(new KeyListener() {
+			public void keyTyped(KeyEvent e) { }
+			public void keyReleased(KeyEvent e) {
+				updateUnattendingEmployeesModel(); // Update unattending model
+			}
+			public void keyPressed(KeyEvent e) { }
+		});
+
+		attendButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				List<Person> selectedEmployees = searchResult.getSelectedValuesList();
+				for (Person p : selectedEmployees) {
+					attendingEmployees.add(p); // Add employee to attending list
+					unattendingEmployees.remove(p); // Remove employee from unattending list
+					if (!potentialEmployeesToAdd.contains(p))
+						potentialEmployeesToAdd.add(p);
+				}
+				updateUnattendingEmployeesModel(); // Update models
+				updateAttendingEmployeesModel();
+			}
+		});
+		undoAttendButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				List<Person> selectedEmployees = attendingList.getSelectedValuesList();
+				for (Person p : selectedEmployees) {
+					unattendingEmployees.add(p);
+					attendingEmployees.remove(p);
+				}
+				updateUnattendingEmployeesModel();
+				updateAttendingEmployeesModel();
+			}
+		});
+	}
+
+	private void addGUI() {
 		// Add elements to grid //
 		GridBagConstraints inputConstraint = new GridBagConstraints();
 		inputConstraint.gridwidth = 3;
@@ -219,28 +258,21 @@ public class Participants extends JDialog {
 		undoAttendConstraint.gridx = 1;
 		undoAttendConstraint.gridy = 3;
 		add(undoAttendButton, undoAttendConstraint);
-		
+
 		GridBagConstraints saveButtonConstraint = new GridBagConstraints();
 		saveButtonConstraint.gridx = 1;
 		saveButtonConstraint.gridy = 4;
 		add(saveButton, saveButtonConstraint);
-		
+
 		setVisible(true);
 	}
 
-	public void updateUnattendingEmployeesModel() {
-		unattendingEmployeesModel.clear(); // Clear model list
-		for (Person p : unattendingEmployees) { // Iterate through selected employees
-			if (! unattendingEmployeesModel.contains(p) && p.getUsername().toLowerCase().contains(searchInput.getText().toLowerCase())) {
-				unattendingEmployeesModel.addElement(p); // Add employee to unattending list if the above requirements are fulfilled
-			}
-		}
-	}
-	
-	public void updateAttendingEmployeesModel() {
-		attendingEmployeesModel.clear();
-		for (Person p : attendingEmployees) {
-			attendingEmployeesModel.addElement(p);
-		}
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Møtedeltagere");
+		frame.setContentPane(new Participants(frame, new Appointment(6), new Person("Kristoffer")));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);	
 	}
 }
